cce2ac20f7fe5e2113c42cd9798c12a06b7246c1
sachinkumarsain <sachinsain21082004@gmail.com>
2024-03-13 18:55:21 +0530

    updating

--- tailwindcss/src/practiceComonents/Header.jsx
+++ tailwindcss/src/practiceComonents/Header.jsx
@@ -1,8 +1,33 @@
+// import React from 'react'
+
+// function Header() {
+//   return (
+//     <header className='flex items-center justify-between p-2 bg-slate-200'>
+//       <h1> Google</h1>
+//       <ul className='flex item-center'>
+//         <li className='px-2'>Home</li>
+//         <li className='px-2'>About</li>
+//         <li className='px-2'>Contact</li>
+//       </ul>
+//       <button>sign Up</button>
+//     </header>
+//   )
+// }
+
+// export default Header
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+        <img src=''/>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        <button>Send</button>
+    </header>
   )
 }
 
